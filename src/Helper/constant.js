3d761aee79f3ef34e9e8f3498bd7b81bceb77037
Umair359 <umairulislamdh359@gmail.com>
2024-01-26 16:54:38 +0500

    a

--- src/Helper/constant.js
+++ src/Helper/constant.js
@@ -1,2 +1,2 @@
-export const ImagebaseUrl = 'https://dc68-39-50-165-45.ngrok-free.app/assets/uploads/'
-// export const ImagebaseUrl = 'https://vida-nexus.azurewebsites.net/assets/uploads/'
+// export const ImagebaseUrl = 'https://dc68-39-50-165-45.ngrok-free.app/assets/uploads/'
+export const ImagebaseUrl = 'https://vida-nexus.azurewebsites.net/assets/uploads/'
